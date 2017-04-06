b6a398dbf8a77f5a2e74df31b07ef25255595db5
Yaroslav Lutsenko <yaroslav.nedoluga@gmail.com>
2017-04-06 12:20:44 +0300

    version 0.0.0 - create new project

---
+++ app/src/main/java/com/gmail/nf/project/jddca/noticefilm/app/MockClass.java
@@ -0,0 +1,9 @@
+package com.gmail.nf.project.jddca.noticefilm.app;
+
+/**
+ * Created by Yaroslav Lutsenko on 06.04.2017.
+ * Mock class for start project architecture
+ */
+
+public class MockClass {
+}
