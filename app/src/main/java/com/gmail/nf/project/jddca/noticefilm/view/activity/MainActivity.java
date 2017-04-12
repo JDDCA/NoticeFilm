6b2f385bda51938fb48fab4ae81c0324e782d47f
Egor Salenko <eg.salenko@gmail.com>
2017-04-12 21:44:15 +0300

    Added MovieFragment to MainActivity

--- app/src/main/java/com/gmail/nf/project/jddca/noticefilm/view/activity/MainActivity.java
+++ app/src/main/java/com/gmail/nf/project/jddca/noticefilm/view/activity/MainActivity.java
@@ -1,21 +1,25 @@
 package com.gmail.nf.project.jddca.noticefilm.view.activity;
 
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.AppCompatButton;
-import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import com.gmail.nf.project.jddca.noticefilm.R;
 import com.gmail.nf.project.jddca.noticefilm.model.utils.FirebaseUtils;
 import com.gmail.nf.project.jddca.noticefilm.presenter.MainPresentr;
+import com.gmail.nf.project.jddca.noticefilm.view.fragment.MovieFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import lombok.experimental.PackagePrivate;
 
 
@@ -29,14 +33,21 @@ public class MainActivity extends FragmentActivity {
     public final String TAG = getClass().getSimpleName();
 
     MainPresentr mainPresentr;
-    @BindView(R.id.logOutGoogle)
+
+    @BindView(R.id.advice_btn)
+    Button btnAdvice;
+
+    @BindView(R.id.genre_spinner)
+    Spinner generesSpinner;
+
+    /*@BindView(R.id.logOutGoogle)
     AppCompatButton logOutGoogle;
     @BindView(R.id.logOutAnonymously)
     AppCompatButton logOutAnonymously;
     @BindView(R.id.showProvider)
     AppCompatButton showProvider;
     @BindView(R.id.convertToGoogle)
-    AppCompatButton convertToGoogle;
+    AppCompatButton convertToGoogle;*/
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -45,10 +56,27 @@ public class MainActivity extends FragmentActivity {
         mainPresentr = new MainPresentr();
         mainPresentr.setActivity(this);
         ButterKnife.bind(this);
-        showProvider.setOnClickListener(v -> Log.i(TAG, "onCreate: "+FirebaseUtils.isAnonymousUser()));
-        logOutGoogle.setOnClickListener(v -> mainPresentr.logout(FirebaseUtils.GOOGLE_PROVIDER));
-        logOutAnonymously.setOnClickListener(v -> mainPresentr.logout(FirebaseUtils.ANONYMOUSLY_PROVIDER) );
-        convertToGoogle.setOnClickListener(v -> {});
+
+        // Фрагмент с фильмом | Fragment with movie info
+        MovieFragment movieFragment = new MovieFragment();
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, movieFragment);
+        transaction.add(R.id.fragment_container, movieFragment);
+        transaction.remove(movieFragment);
+        transaction.addToBackStack(null);
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        transaction.commit();
+
+        // Создание spinner'а и присоединение адаптера с жанрами | Creating Spinner with genres adapter
+        ArrayAdapter<CharSequence> adapterGenres = ArrayAdapter.createFromResource(this, R.array.genres, android.R.layout.simple_spinner_item);
+        generesSpinner.setAdapter(adapterGenres);
+
+//        showProvider.setOnClickListener(v -> Log.i(TAG, "onCreate: "+FirebaseUtils.isAnonymousUser()));
+//        logOutGoogle.setOnClickListener(v -> mainPresentr.logout(FirebaseUtils.GOOGLE_PROVIDER));
+//        logOutAnonymously.setOnClickListener(v -> mainPresentr.logout(FirebaseUtils.ANONYMOUSLY_PROVIDER) );
+//        convertToGoogle.setOnClickListener(v -> {});
+
+
     }
 
     @Override
@@ -58,15 +86,20 @@ public class MainActivity extends FragmentActivity {
         checkSession();
     }
 
-    public static Intent createIntent (@NonNull Activity activity){
+    public static Intent createIntent(@NonNull Activity activity) {
         return new Intent(activity, MainActivity.class);
     }
 
-    private void checkSession (){
-        if (!FirebaseUtils.checkSession()){
+    private void checkSession() {
+        if (!FirebaseUtils.checkSession()) {
             startActivity(LoginActivity.createIntent(this));
             finish();
         }
+    }
+
+    @OnClick(R.id.advice_btn)
+    public void onClick(View view) {
+// Реализовать логику нажатия на кнопку | Implement logic press on button
     }
 
 
