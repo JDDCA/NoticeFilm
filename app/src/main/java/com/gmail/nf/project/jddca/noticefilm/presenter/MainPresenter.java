18f87ee7c27949877d97191f413fe9ffcc9870d7
Egor Salenko <eg.salenko@gmail.com>
2017-04-13 22:53:26 +0300

    implement method getMovie() with RxJava

--- app/src/main/java/com/gmail/nf/project/jddca/noticefilm/presenter/MainPresenter.java
+++ app/src/main/java/com/gmail/nf/project/jddca/noticefilm/presenter/MainPresenter.java
@@ -1,11 +1,22 @@
 package com.gmail.nf.project.jddca.noticefilm.presenter;
 
-import android.content.Intent;
+import android.util.Log;
 
 import com.gmail.nf.project.jddca.noticefilm.model.DataManager;
+import com.gmail.nf.project.jddca.noticefilm.model.pojos.Movie;
+import com.gmail.nf.project.jddca.noticefilm.model.rest.MovieService;
+import com.gmail.nf.project.jddca.noticefilm.model.rest.MovieServiceImpl;
 import com.gmail.nf.project.jddca.noticefilm.view.activity.MainActivity;
+import com.gmail.nf.project.jddca.noticefilm.view.fragment.MovieFragment;
 
-import lombok.NonNull;
+import javax.inject.Inject;
+
+import dagger.Module;
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 import lombok.Setter;
 import lombok.experimental.PackagePrivate;
 
@@ -14,6 +25,8 @@ public class MainPresenter {
 
     DataManager dataManager;
 
+    MovieService movieService = new MovieServiceImpl();
+
     @Setter
     MainActivity activity;
 
@@ -21,9 +34,39 @@ public class MainPresenter {
         dataManager = DataManager.getInstance();
     }
 
-    public void logout (int PROVIDER){
-            dataManager.logout(activity,PROVIDER);
+    public void logout(int PROVIDER) {
+        dataManager.logout(activity, PROVIDER);
     }
 
+    public void getMovie(long id, String key) {
+
+        Observable<Movie> movieObservable = movieService.get(id, key);
+
+        movieObservable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Movie>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(Movie movie) {
+                Log.e("MainPresenter", movie.getTitle() + " " + movie.getOverview() + " " + movie.getReleaseDate());
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e("MainPresenter", e.toString());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.e("MainPresenter", "Success");
+            }
+        });
+
+    }
 
 }
