b9c97e6692cc62ce8dcf82e403b42609b124b60e
Egor Salenko <eg.salenko@gmail.com>
2017-04-23 21:47:44 +0300

    new Presenter for Upcoming Movies

---
+++ app/src/main/java/com/gmail/nf/project/jddca/noticefilm/presenter/UpcomingMoviePresenter.java
@@ -0,0 +1,36 @@
+package com.gmail.nf.project.jddca.noticefilm.presenter;
+
+
+import com.gmail.nf.project.jddca.noticefilm.model.rest.UpcomingMovieService;
+import com.gmail.nf.project.jddca.noticefilm.model.rest.UpcomingMovieServiceImpl;
+import com.gmail.nf.project.jddca.noticefilm.model.utils.ApiService;
+import com.gmail.nf.project.jddca.noticefilm.model.utils.RetrofitService;
+import com.gmail.nf.project.jddca.noticefilm.view.IView;
+import com.gmail.nf.project.jddca.noticefilm.view.UpcomingView;
+import com.gmail.nf.project.jddca.noticefilm.view.fragment.UpcomingMovieFragment;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+import lombok.Setter;
+
+public class UpcomingMoviePresenter implements Presenter {
+
+    @Setter
+    private UpcomingMovieFragment fragment;
+
+    private UpcomingView view;
+    private UpcomingMovieService service;
+
+    public UpcomingMoviePresenter(IView view) {
+        service = new UpcomingMovieServiceImpl(RetrofitService.getRetrofit());
+        this.view = (UpcomingView) view;
+    }
+
+    public void getUpcoming() {
+        service.getUpcomingMovie(ApiService.getApiKey(fragment.getContext()), ApiService.getLocales(fragment.getContext()), 1)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(movies -> view.showUpcomingMoviesList(movies),
+                        throwable -> view.showError(throwable));
+    }
+}
